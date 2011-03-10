f0599439b9e60120567f5944a9dfd0821f19abe2
Roman Kamyk <roman.kamyk@gmail.com>
2011-03-10 11:42:10 +0100

    template files

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,17 @@ license          "MIT/BSD"
 description      "Configures notifications for mercurial-server repositories"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+%w(
+  postfix
+  mercurial-server
+).each do |d|
+  depends d
+end
+
+%w(
+  ubuntu
+).each do |os|
+  supports os
+end
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,19 @@
 # Copyright 2011, Itiner
 #
 
+package "procmail"
+
+template "/etc/mercurial-server/remote-hgrc.d/notify.rc" do
+  source "notify.rc"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "/etc/mercurial-server/styles/notify.style" do
+  source "notify.style"
+  owner "root"
+  group "root"
+  mode "0644"
+end
 
