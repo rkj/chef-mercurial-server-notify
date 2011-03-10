19eddd881614d30fd044adba7949b4d1279ca724
Roman Kamyk <roman.kamyk@gmail.com>
2011-03-10 10:45:25 +0100

    init

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Itiner"
+maintainer_email "roman.kamyk@itiner.pl"
+license          "MIT/BSD"
+description      "Configures notifications for mercurial-server repositories"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,7 @@
+# Cookbook Name:: mercurial-server-notify
+# Recipe:: default
+#
+# Copyright 2011, Itiner
+#
+
+
